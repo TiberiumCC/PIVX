ef94b5bed4827e647d6f87aa36c68cbe922e9e1f
TiberiumCC <flestreyd@gmail.com>
2018-03-21 03:00:40 +0300

    Change ports in main and test network: 7125 - main and 7126 - test.

--- src/qt/locale/tiberium_en.ts
+++ src/qt/locale/tiberium_en.ts
@@ -6284,8 +6284,8 @@ for example: alertnotify=echo %%s | mail -s &quot;TIBERIUM Alert&quot; admin@foo
     </message>
     <message>
         <location line="+3"/>
-        <source>(51472 could be used only on mainnet)</source>
-        <translation type="unfinished">(51472 could be used only on mainnet)</translation>
+        <source>(7125 could be used only on mainnet)</source>
+        <translation type="unfinished">(7125 could be used only on mainnet)</translation>
     </message>
     <message>
         <location line="+1"/>
@@ -6299,7 +6299,7 @@ for example: alertnotify=echo %%s | mail -s &quot;TIBERIUM Alert&quot; admin@foo
     </message>
     <message>
         <location line="+1"/>
-        <source>(must be 51472 for mainnet)</source>
+        <source>(must be 7125 for mainnet)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -176,7 +176,7 @@ UniValue addnode(const UniValue& params, bool fHelp)
             "1. \"node\"     (string, required) The node (see getpeerinfo for nodes)\n"
             "2. \"command\"  (string, required) 'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once\n"
             "\nExamples:\n" +
-            HelpExampleCli("addnode", "\"192.168.0.6:51472\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:51472\", \"onetry\""));
+            HelpExampleCli("addnode", "\"192.168.0.6:7125\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:7125\", \"onetry\""));
 
     string strNode = params[0].get_str();
 
